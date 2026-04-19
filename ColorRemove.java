d25667283f6fabd627717dbcf865ba309095dd90
MANOJ TALAWAR <manojtalawar835@gmail.com>
2026-04-19 17:33:55 +0530

    Update ColorRemove.java

--- ColorRemove.java
+++ ColorRemove.java
@@ -1,3 +1,8 @@
+/* 2. Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList
+    */
+
+
 package ArrayList;
 
 import java.util.ArrayList;
