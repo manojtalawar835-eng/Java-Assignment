a141f93895932f402013cd44864e99cc19393ad9
MANOJ TALAWAR <manojtalawar835@gmail.com>
2026-04-19 17:25:18 +0530

    Add files via upload

---
+++ ColorSearch.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ColorSearch {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        if (colors.contains("Red")) {
+            System.out.println("Color Red is available.");
+        } else {
+            System.out.println("Color Red is not available.");
+        }
+    }
+}
