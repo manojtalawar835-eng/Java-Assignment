68245546a37b1fc933dfbf44cdc018f6e081a100
MANOJ TALAWAR <manojtalawar835@gmail.com>
2026-04-19 17:36:51 +0530

    Update ColorSort.java

--- ColorSort.java
+++ ColorSort.java
@@ -1,3 +1,9 @@
+/* 3. Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)
+    */
+
+
+
 package ArrayList;
 import java.util.ArrayList;
 import java.util.Collections;
