4e785f41ae49ec1c1ffff305ae799af199501ba3
MANOJ TALAWAR <manojtalawar835@gmail.com>
2026-04-19 17:40:43 +0530

    Add comment explaining color extraction using subList
    Updated the ColorSubList.java file to include a comment explaining the purpose of the program.

--- ColorSubList.java
+++ ColorSubList.java
@@ -1,3 +1,7 @@
+/*  4. Write a java program for getting different colors through ArrayList interface and extract the
+elements 1st and 2nd from the ArrayList object by using SubList()
+    */
+
 package ArrayList;
 
 import java.util.ArrayList;
