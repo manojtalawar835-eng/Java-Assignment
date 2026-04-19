50a9153c15a72f745b935749c705c85c22a777e5
MANOJ TALAWAR <manojtalawar835@gmail.com>
2026-04-19 17:41:05 +0530

    Fix formatting and comments in ColorDeleteNth.java

--- ColorDeleteNth.java
+++ ColorDeleteNth.java
@@ -1,4 +1,4 @@
-/* Write a java program for getting different colors through ArrayList interface and delete nth
+/*  5. Write a java program for getting different colors through ArrayList interface and delete nth
 element from the ArrayList object by using remove by index
     */
 
