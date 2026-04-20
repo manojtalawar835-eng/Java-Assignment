234c779093b15186774886f0ecb1802218b1d262
MANOJ <MANOJ@LAPTOP-C87M54KH>
2026-04-20 17:02:56 +0530

    commited

---
+++ src/main/java/com/Cookies/CookieServlet.java
@@ -0,0 +1,68 @@
+/* 8a. Build a servlet program to create a cookie to get your name through text box and press submit button(
+through HTML) to display the message by greeting Welcome back your name ! , you have visited this page
+n times ( n = number of your visit ) along with the list of cookies and demonstrate the expiry of cookie also.
+*/
+
+
+package com.Cookies;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/CookieServlet")
+public class CookieServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+
+        int visitCount = 1;
+        Cookie[] cookies = request.getCookies();
+
+        // Check existing cookies
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue());
+                    visitCount++;
+                }
+            }
+        }
+
+        // Create/update visit cookie
+        Cookie visitCookie = new Cookie("visitCount", String.valueOf(visitCount));
+        visitCookie.setMaxAge(30); // expiry 30 sec
+        response.addCookie(visitCookie);
+
+        // Create username cookie
+        Cookie nameCookie = new Cookie("username", username);
+        nameCookie.setMaxAge(30);
+        response.addCookie(nameCookie);
+
+        out.println("<html><body>");
+
+        // Greeting message
+        out.println("<h2>Welcome back " + username + "!</h2>");
+        out.println("<p>You have visited this page " + visitCount + " times.</p>");
+        out.println("<p><b>Cookie will expire in 30 seconds</b></p>");
+
+        // Display list of cookies (8a requirement)
+        out.println("<h3>List of Cookies:</h3>");
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie c : cookies) {
+                out.println("<p>" + c.getName() + "</p>");
+            }
+        } else {
+            out.println("<p>No cookies found</p>");
+        }
+
+        out.println("</body></html>");
+    }
+}
